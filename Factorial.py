743fca973c9c53ac252443b7cba4fea102851b17
Veer Singh <singhdalveer247@gmail.com>
2023-04-02 17:49:50 +0530

    Find Factorial

---
+++ Factorial.py
@@ -0,0 +1,16 @@
+def fact(val):
+    firstval = 1
+    secondval = 2
+    if val <= 0:
+        print('Please enter the value greater than 0!')
+    elif val == 1:
+        print(firstval)
+    else:
+        for i in range(1, val):
+            result = firstval * secondval
+            firstval = result 
+            secondval += 1
+        print(result)
+
+num = int(input("Enter the value you want to find the factorial of: "))
+fact(num)
