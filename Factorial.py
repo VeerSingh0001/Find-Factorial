b6dfa0a8e81e937b01986a1619101a360f633d75
Veer Singh <singhdalveer247@gmail.com>
2023-04-06 13:49:08 +0530

    Factorial

--- Factorial.py
+++ Factorial.py
@@ -8,11 +8,9 @@ def fact(val):
     elif val == 1:
         print(firstval)
     else:
-        for i in range(1, val):
-            result = firstval * secondval
-            firstval = result 
-            secondval += 1
-        print(result)
+        for i in range(1, val+1):
+            firstval = firstval * i
+        print(firstval)
 
 num = int(input("Enter the value you want to find the factorial of: "))
 fact(num)
